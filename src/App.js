a91ec3df2db664848d8bf4e92c7d589778cb9c94
pandeyprashant953 <pandeyprashant953@gmail.com>
2023-01-10 14:38:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Login from './component/Login';
-// import Register from './component/Register';
+// import Login from './component/Login';
+import Register from './component/Register';
 // import FormChallenge from './component/FormChallenge';
 
 function App() {
   return (
     <>
-      <Login />
-      {/* <Register /> */}
+      {/* <Login /> */}
+      <Register />
       {/* <FormChallenge /> */}
     </>
   );
